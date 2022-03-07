5c35449056047b52c6e59515556572dcd2997ad4
PlinioAbner <plinio414@gmail.com>
2022-03-07 17:03:29 -0500

    initial commit

---
+++ VariableExerciseConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VariablesExerciseConsole
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Variable Delcarations
+            int age;
+            string name;
+
+            bool isPuertorican;
+            Decimal weight;
+            
+
+
+            //Variable initalization
+            age = 24;
+            name = "Plinio";
+            isPuertorican = true;
+            weight = 150.23m;
+
+            string fullname = "Plinio Colon";
+            
+
+            Console.WriteLine($"{name} age is {age }. is he puerorican?: {isPuertorican}.The weight of {fullname} is: {weight}lb");
+
+            
+        }
+    }
+
+
+}
